1241c381f5b29fd6901a2bf0239019a6532bc08f
imd6 <ibnumudha@gmail.com>
2021-08-02 14:12:49 +0800

    Update autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -8,4 +8,6 @@ nm-applet &
 #powerkit &
 #cbatticon -r 10 -l 20 -i symbolic &
 mate-power-manager &
+/usr/bin/emacs --daemon &
+notify-send "Welcome home, $USER"
 #redshift -l -5.115309:119.518395 -t 4000:4000 &
